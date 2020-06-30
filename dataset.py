2e9f48fe157f2a7028a3b5602157f4aec5a7b049
Paolo Grasso <paolo.1995@ymail.com>
2020-06-30 09:48:53 +0200

    Update model

--- dataset.py
+++ dataset.py
@@ -6,7 +6,6 @@ import numpy as np
 import librosa.display
 import pretty_midi as pm
 import copy
-import time
 
 
 def transpose(data):
@@ -38,7 +37,7 @@ def plot_piano_roll(pr, start_pitch, end_pitch, ax, fs=100):
 class DataGenerator:
     """Class to yield midi_file as batches."""
 
-    def __init__(self, midi_list, path, fs=4, quant=0, binarize=1):
+    def __init__(self, midi_list, path, fs=4, quant=0, binarize=1, bl=0):
         """Initialise list."""
         self.midi_list = midi_list
         self.dim = len(midi_list)
@@ -46,6 +45,7 @@ class DataGenerator:
         self.quant = quant
         self.binar = binarize
         self.path = path
+        self.baseline = bl
 
     def binarize(self, pr):
         """Binarize data."""
@@ -63,12 +63,75 @@ class DataGenerator:
 
         return obj
 
-    def generate(self, limit=None):
+    def pitch_transpose(self, ds, value, timesteps=None):
+        """Perform pitch transposition."""
+        if timesteps:
+            range_timesteps = ds.shape[1]
+            for t in range(range_timesteps):
+                d = pd.DataFrame(ds[:, t, :])
+                d.shift(value, axis=1, fill_value=0)
+                ds[:, t, :] = d.values
+        else:
+            d = pd.DataFrame(ds)
+            d.shift(value, axis=1, fill_value=0)
+            ds = d.values
+
+        return ds
+
+    def generate(self, bs, limit=None, trans=None, name=''):
         """Yield a dataset."""
+        dim = self.dataset[0].shape[0]
+        batch = bs
+        epoch = 0
         while True:
-            yield self.dataset
-            if limit == 1:
-                break
+            cnt = 0
+            epoch += 1
+            en = 0
+            c = 0
+            while en < dim:
+                en = c + batch
+                yield (self.dataset[0][c:en], self.dataset[1][c:en])
+                c += batch
+                cnt += 1
+            if trans == 1:
+                for v in [-5, -4, -3, -2, -1, 1, 2, 3, 4, 5]:
+                    x = self.pitch_transpose(self.dataset[0], v, 1)
+                    y = self.pitch_transpose(self.dataset[1], v)
+                    en = 0
+                    c = 0
+                    while en < dim:
+                        en = c + batch
+                        yield (x[c:en], y[c:en])
+                        c += batch
+                        cnt += 1
+            print("\nEpoch %d finished for %s, %d" % (epoch, name, cnt))
+            # if limit == 1:
+            #     break
+
+    def generate_transposed(self):
+        """Create a generator for transposed dataset."""
+        # for v in [-5, -4, -3, -2, -1, 1, 2, 3, 4, 5]:
+        for v in [-5, -3, -1, 1, 3, 5]:
+            x = self.pitch_transpose(self.dataset[0], v, 1)
+            y = self.pitch_transpose(self.dataset[1], v)
+            yield (x, y)
+
+    def build_transposed(self):
+        """Build a transposed dataset."""
+        generator = self.generate_transposed()
+        # print(self.dataset[0].shape)
+        x_data = self.dataset[0]
+        y_target = self.dataset[1]
+
+        for i in range(6):
+            (x, y) = next(generator)
+
+            # Append transposed data.
+            x_data = np.concatenate((x_data, x), axis=0)
+            # Append transposed target.
+            y_target = np.concatenate((y_target, y), axis=0)
+
+        self.dataset = (x_data, y_target)
 
     def build_dataset(self, name, step=1, t_step=1):
         """Build a dataset."""
@@ -86,26 +149,38 @@ class DataGenerator:
             if self.binar:
                 prt = self.binarize(prt)
 
-            target = prt[step:, :]
-            data = prt[:-t_step, :]
-            # target = prt
-            # data = prt
+            if self.baseline:
+                data = prt[:-t_step, :]
+                target = prt[step-1:-1, :]
+
+            else:
+                data = prt[:-t_step, :]
+                target = prt[step:, :]
 
             if step > 0:
                 data_new = []
                 for c in range(len(data)-step+1):
                     conc = np.array([data[x] for x in range(c, c+step)])
                     data_new.append(conc)
-                    # data_new.append(np.concatenate(conc, axis=None))
 
                 data = np.array(data_new)
 
             if t_step > 0:
                 target_new = []
-                for c in range(len(target)-t_step+1):
-                    conc = np.array([target[x] for x in range(c, c+t_step)])
-                    # target_new.append(conc)
-                    target_new.append(np.concatenate(conc, axis=None))
+
+                if self.baseline:
+                    for c in range(len(target)-t_step+1):
+                        conc = np.array([target[c] for x in range(c,
+                                                                  c+t_step)])
+
+                        target_new.append(np.concatenate(conc, axis=None))
+
+                else:
+                    for c in range(len(target)-t_step+1):
+                        conc = np.array([target[x] for x in range(c,
+                                                                  c+t_step)])
+                        target_new.append(np.concatenate(conc, axis=None))
+
                 target = np.array(target_new)
 
             if flag == 0:
@@ -123,9 +198,6 @@ class DataGenerator:
                 target = copy.copy(b)
 
         self.dataset = ((data, target))
-        # print(data.shape)
-        # print(target.shape)
-        # time.sleep(50)
         self.dime = len(data)
 
 
--- model.py
+++ model.py
@@ -4,6 +4,7 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Flatten
+# from tensorflow.keras.layers import Dropout
 # from tensorflow.keras.layers import SimpleRNN
 import tensorflow as tf
 import keras.metrics
@@ -11,6 +12,7 @@ import keras.backend as K
 
 
 def build_model(inp_shape, num_ts):
+    num_ts = int(num_ts)
     """Create Keras model."""
     # Allow memory growth.
     physical_devices = tf.config.list_physical_devices('GPU')
@@ -18,10 +20,10 @@ def build_model(inp_shape, num_ts):
 
     # Create the model.
     model = Sequential()
-    model.add(Dense(64, input_shape=(inp_shape),  activation='relu'))
+    model.add(Dense(32, input_shape=(inp_shape),  activation='relu'))
     model.add(Flatten())
-    model.add(Dense(64, activation='relu'))
-    model.add(Dense(880, activation='sigmoid', name='Output'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(88*num_ts, activation='sigmoid', name='Output'))
 
     # opt = tf.keras.optimizers.SGD(learning_rate=0.1)
 
@@ -29,8 +31,7 @@ def build_model(inp_shape, num_ts):
                   optimizer='adam',
                   metrics=[keras.metrics.Precision(),
                            keras.metrics.Recall(),
-                           # keras.metrics.AUC(),
-                           f1_t1, f1_t10])
+                           f1_first, f1_last])
 
     return model
 
@@ -49,10 +50,11 @@ def f1(y_true, y_pred):
     return f1_val
 
 
-def f1_t10(y_true, y_pred):
+def f1_last(y_true, y_pred):
     """Compute F1-score metric on a portion of the output."""
-    y_true = y_true[-88:]
-    y_pred = y_pred[-88:]
+    # Select the last 88 notes.
+    y_true = y_true[:, -88:]
+    y_pred = y_pred[:, -88:]
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
     predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
@@ -62,10 +64,124 @@ def f1_t10(y_true, y_pred):
     return f1_val
 
 
-def f1_t1(y_true, y_pred):
+def f1_first(y_true, y_pred):
     """Compute F1-score metric on a portion of the output."""
-    y_true = y_true[:88]
-    y_pred = y_pred[:88]
+    # Select the first 88 notes.
+    y_true = y_true[:, 0:88]
+    y_pred = y_pred[:, 0:88]
+
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    recall = true_positives / (possible_positives + K.epsilon())
+    f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
+    return f1_val
+
+
+def f1_2(y_true, y_pred):
+    """Compute F1-score metric on a portion of the output."""
+    # Select the second 88 notes.
+    y_true = y_true[:, 88:176]
+    y_pred = y_pred[:, 88:176]
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    recall = true_positives / (possible_positives + K.epsilon())
+    f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
+    return f1_val
+
+
+def f1_3(y_true, y_pred):
+    """Compute F1-score metric on a portion of the output."""
+    # Select the second 88 notes.
+    y_true = y_true[:, 88*2:88*3]
+    y_pred = y_pred[:, 88*2:88*3]
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    recall = true_positives / (possible_positives + K.epsilon())
+    f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
+    return f1_val
+
+
+def f1_4(y_true, y_pred):
+    """Compute F1-score metric on a portion of the output."""
+    # Select the second 88 notes.
+    y_true = y_true[:, 88*3:88*4]
+    y_pred = y_pred[:, 88*3:88*4]
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    recall = true_positives / (possible_positives + K.epsilon())
+    f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
+    return f1_val
+
+
+def f1_5(y_true, y_pred):
+    """Compute F1-score metric on a portion of the output."""
+    # Select the second 88 notes.
+    y_true = y_true[:, 88*4:88*5]
+    y_pred = y_pred[:, 88*4:88*5]
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    recall = true_positives / (possible_positives + K.epsilon())
+    f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
+    return f1_val
+
+
+def f1_6(y_true, y_pred):
+    """Compute F1-score metric on a portion of the output."""
+    # Select the second 88 notes.
+    y_true = y_true[:, 88*5:88*6]
+    y_pred = y_pred[:, 88*5:88*6]
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    recall = true_positives / (possible_positives + K.epsilon())
+    f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
+    return f1_val
+
+
+def f1_7(y_true, y_pred):
+    """Compute F1-score metric on a portion of the output."""
+    # Select the second 88 notes.
+    y_true = y_true[:, 88*6:88*7]
+    y_pred = y_pred[:, 88*6:88*7]
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    recall = true_positives / (possible_positives + K.epsilon())
+    f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
+    return f1_val
+
+
+def f1_8(y_true, y_pred):
+    """Compute F1-score metric on a portion of the output."""
+    # Select the second 88 notes.
+    y_true = y_true[:, 88*7:88*8]
+    y_pred = y_pred[:, 88*7:88*8]
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    recall = true_positives / (possible_positives + K.epsilon())
+    f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
+    return f1_val
+
+
+def f1_9(y_true, y_pred):
+    """Compute F1-score metric on a portion of the output."""
+    # Select the second 88 notes.
+    y_true = y_true[:, 88*8:88*9]
+    y_pred = y_pred[:, 88*8:88*9]
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
     predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
